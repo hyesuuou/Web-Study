9983933bfb9fa6f02b70a29fec5332df31506fc8
hyesuuou <68391767+hyesuuou@users.noreply.github.com>
2021-01-19 15:16:36 +0900

    egoing : WEB2-Node.js 14-22 practics

---
+++ Code/egoing-web-nodejs/syntax/comparison.js
@@ -0,0 +1,8 @@
+// 비교연산자
+console.log(1==1); // true
+console.log(1==2); // false
+console.log(1>2); // false
+console.log(1<2); // true
+console.log(1===1); // true
+console.log(1===2); // false
+// ===는 두개(==)를 쓸 이류가 없으면 3개(===)를 쓰도록 하자.
