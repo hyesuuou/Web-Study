677ff4edb64e1ffa3c07d1303c1d801ee4949c14
hyesuuou <68391767+hyesuuou@users.noreply.github.com>
2021-01-20 23:32:15 +0900

    egoing : WEB2-Node.js 39-42 practice

--- Code/egoing-web-nodejs/syntax/Object3.js
+++ Code/egoing-web-nodejs/syntax/Object3.js
@@ -3,10 +3,10 @@ var o = {
   v1: 'v1',
   v2: 'v2',
   f1: function (){
-    console.log(v1);
+    console.log(this.v1);
   }
   f2: function (){
-    console.log(v2);
+    console.log(this.v2);
   }
 }
 
