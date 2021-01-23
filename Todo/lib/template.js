b0db198b324d4aa1e3ad64292e2cdefb8e24ebec
hyesuuou <68391767+hyesuuou@users.noreply.github.com>
2021-01-23 18:26:03 +0900

    feat : Add home & print daily todo

---
+++ Todo/lib/template.js
@@ -0,0 +1,79 @@
+module.exports = {
+  list : function (fileList){
+    var list = '<ul>';
+    var i = 0;
+    while (i < fileList.length){
+      list = list + `<li><a href="/?id=${fileList[i]}">${fileList[i]}</a></li>`;
+      i=i+1;
+    }
+
+    list = list + '</ul>'
+    return list;
+  },
+
+  homehtml : function(list){
+    return `
+    <!doctype html>
+    <html>
+    <head>
+      <title>Todo-Home</title>
+      <meta charset="utf-8">
+    </head>
+    <body>
+      ${list}
+    </body>
+    </html>
+    `;
+  },
+  dailyhtml : function(date,body){
+    // 지금 이거 dailyto.html 복붙한거임
+    return `
+    <!DOCTYPE html>
+    <html lang="en" dir="ltr">
+      <head>
+        <meta charset="utf-8">
+        <title>To-do Daily</title>
+        <link rel="stylesheet" href="../dailytodo.css">
+
+      </head>
+      <body>
+        <div class="check_page">
+          <a href="dailytodo.html" style="color:powderblue">Daily</a>
+          <span class="tab">&#9;</span>
+          <a href="weektodo.html">Weekly</a>
+          <span class="tab">&#9;</span>
+          <a href="monthtodo.html">Monthly</a>
+        </div>
+
+        <div class=left>
+
+        </div>
+
+        <div class="center">
+          <div class="center_date">
+            <div id="today_date">
+              ${date}
+            </div>
+          </div>
+          <div class="center_top">
+            <input id="input_todo" type="text" placeholder="할 일을 입력하세요.">
+            <input id="input_todo_button" type="button" value="추가">
+          </div>
+
+          <div class="center_bottom">
+            <ul id="todo_list">
+              ${body}
+            </ul>
+          </div>
+        </div>
+
+        <div class="right">
+
+        </div>
+
+      </body>
+      <script src="script.js"></script>
+    </html>
+    `;
+  }
+}
---
+++ Todo/main.js
@@ -0,0 +1,63 @@
+var http = require('http');
+var fs = require('fs');
+var url = require('url');
+var qs = require('querystring');
+var path = require('path');
+var template = require('./lib/template.js');
+
+
+var app = http.createServer(function(request, response){
+  var _url = request.url;
+  var queryData = url.parse(_url, true).query;
+  var pathname = url.parse(_url, true).pathname;
+  console.log(url.parse(_url,true));
+
+  if (pathname === '/'){
+    if (queryData.id === undefined){  //id값 없으면 (home!) -> 사용법daily, weekly, monthly 모아보기 & 날짜 목록 출력할것
+      fs.readdir('./data', function(error, fileList){
+        var list = template.list(fileList);
+        var html = template.homehtml(list);
+        response.writeHead(200);
+        response.end(html);
+      })
+
+
+    }
+    else {  //id값 있으면 (id값을 daily, weekly, monthly로 할지... 아니면 날짜로 할지 매우 고민중)
+      fs.readdir('./data', function(error, fileList){
+        var filteredId = path.parse(queryData.id).base;
+        var date = queryData.id;
+        fs.readFile(`data/${filteredId}`, 'utf8', function(err, description){
+          var html = template.dailyhtml(date, description);
+          response.writeHead(200);
+          response.end(html);
+        })
+      });
+    }
+
+  }
+
+  else if (pathname === '/create'){   // 날짜 생성 양식 출력 !
+
+  }
+
+  else if (pathname === '/create_process'){   // 날짜 생성 버튼 누른 후!
+
+  }
+
+  else if (pathname === '/dailytodo_create_process'){ // 해당 날짜 할일 추가 버튼 누른 후
+
+  }
+
+  else if (pathname === '/delete_process'){   // 날짜 삭제
+
+  }
+
+  else {  // Not found 페이지
+    response.writeHead(404);
+    response.end('Not found');
+  }
+
+});
+
+app.listen(3000);
