2e798d9d6a12de28c44d4680ab95290c0436b024
hyesuuou <68391767+hyesuuou@users.noreply.github.com>
2021-02-11 23:26:53 +0900

    nodejs-tb : multer

--- Nodejs-Textbook/Code/Chapter06/6.2/learn-express/app.js
+++ Nodejs-Textbook/Code/Chapter06/6.2/learn-express/app.js
@@ -10,7 +10,7 @@ const app = express();
 app.set('port', process.env.PORT || 3000);
 
 // 미들웨어 사용 (요청과 응답의 중간에 위치하여 이를 조작하여 기능을 추가하거나 나쁜 요청을 걸러내는 역할을 한다.)
-app.use(morgan('combined'));
+app.use(morgan('dev'));
 app.use('/', express.static(path.join(__dirname, 'public')));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false}));
@@ -25,6 +25,42 @@ app.use(session({
     },
     name: 'session-cookie',
 }));
+
+const multer = require('multer');
+const fs = require('fs');
+
+try {
+    fs.readFileSync('uploads');
+}
+catch(err){
+    console.error('uploads 폴더가 없어 uploads 폴더를 생성합니다.');
+    fs.mkdirSync('uploads');
+}
+
+const upload = multer({
+    storage: multer.diskStorage({
+        destination(req, file, done){
+            done(null, 'uploads/');
+        },
+        filename(req, file, done){
+            const ext = path.extname(file.originalname);
+            done(null, path.basename(file.originalname, ext) + Date.now() + ext);
+        },
+    }),
+    limits: { fileSize: 5*1024*1024 },
+});
+app.get('/upload', (req, res)=>{
+    res.sendFile(path.join(__dirname, 'multipart.html'));
+});
+
+app.post('/upload', 
+    upload.fields([ { name: 'image1'}, { name: 'image2'}]),
+    (req, res) => {
+        console.log(req.files, req.body);
+        res.send('ok');
+    },
+);
+
 
 app.use((req, res, next)=>{
     console.log('모든 요청에 다 실행됩니다.');
