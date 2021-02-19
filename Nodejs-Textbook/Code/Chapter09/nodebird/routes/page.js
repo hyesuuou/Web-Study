f437af68fba60166bd81275443d926fa741c207f
hyesuuou <68391767+hyesuuou@users.noreply.github.com>
2021-02-20 01:24:48 +0900

    nodejs-tb : NodeBird - multer 이미지 업로드

--- Nodejs-Textbook/Code/Chapter09/nodebird/routes/page.js
+++ Nodejs-Textbook/Code/Chapter09/nodebird/routes/page.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const { isLoggedIn, isNotLoggedIn } = require('./middlewares');
+const { Post, User } = require('../models');
 
 const router = express.Router();
 
@@ -22,11 +23,25 @@ router.get('/join', isNotLoggedIn, (req,res)=>{
 });
 
 router.get('/', (req, res, next)=>{
-    const twits = [];
-    res.render('main', {
-        title: 'NodeBird',
-        twits,
-    });
+    try{
+        const posts = await Post.findAll({
+            include: {
+                model: User,
+                attributes: ['id', 'nick'],
+            },
+            order: [['createdAt', 'DESC']],
+        });
+        res.render('main', {
+            title: 'NodeBird',
+            twits: posts,
+        });
+    }
+    catch (error){
+        console.error(error);
+        next(error);
+    }
+    
+    
 });
 
 module.exports = router;
