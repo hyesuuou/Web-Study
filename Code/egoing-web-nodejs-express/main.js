01a52d4078e2ea04a810aee67117b417114cdd53
hyesuuou <68391767+hyesuuou@users.noreply.github.com>
2021-02-25 22:18:22 +0900

    egoing : WEB3-Express 9.2-13 practice

--- Code/egoing-web-nodejs-express/main.js
+++ Code/egoing-web-nodejs-express/main.js
@@ -5,37 +5,52 @@ var path = require('path');
 var qs = require('querystring');
 var bodyParser = require('body-parser')
 var sanitizeHtml = require('sanitize-html');
+var compression = require('compression');
 const template = require('./lib/template.js');
 
 const app = express()
 const port = 3000
 
-app.use(bodyParser.urlencoded({ extended: false }))
+app.use(express.static('public')); // public 디렉토리 안의 정적 파일 사용하겠다.
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(compression());
+
+// 미들웨어 만들기
+app.get('*', function(request, response, next){
+  fs.readdir('./data', function(error, fileList){
+    request.list = fileList;
+    next();
+  });
+});
+
 
 // route, routing -> path 마다 적당한 응답을 해줌.
 app.get('/', (request, response) => {
-  fs.readdir('./data', function(error, filelist){
-    var title = 'Welcome';
-    var description = 'Hello, Node.js';
-    var list = template.list(filelist);
-    var html = template.HTML(title, list,
-      `<h2>${title}</h2>${description}`,
-      `<a href="/create">create</a>`
-    );
-    response.send(html);
-  })
+  var title = 'Welcome';
+  var description = 'Hello, Node.js';
+  var list = template.list(request.list);
+  var html = template.HTML(title, list,
+    `<h2>${title}</h2>${description}
+    <img src="/images/hello.jpg" style="width:400px; display:block; margin-top:10px">
+    ` ,
+    `<a href="/create">create</a>`
+  );
+  response.send(html);
 })
 
-app.get('/page/:pageId', (request, response) => {
-  fs.readdir('./data', function(error, filelist){
-    var filteredId = path.parse(request.params.pageId).base;
-    fs.readFile(`data/${filteredId}`, 'utf8', function(err, description){
+app.get('/page/:pageId', (request, response, next) => {
+  var filteredId = path.parse(request.params.pageId).base;
+  fs.readFile(`data/${filteredId}`, 'utf8', function(err, description){
+    if (err){
+      next(err);
+    }
+    else {
       var title = request.params.pageId;
       var sanitizedTitle = sanitizeHtml(title);
       var sanitizedDescription = sanitizeHtml(description, {
-        allowedTags:['h1']
+       allowedTags:['h1']
       });
-      var list = template.list(filelist);
+      var list = template.list(request.list);
       var html = template.HTML(sanitizedTitle, list,
         `<h2>${sanitizedTitle}</h2>${sanitizedDescription}`,
         ` <a href="/create">create</a>
@@ -46,28 +61,28 @@ app.get('/page/:pageId', (request, response) => {
           </form>`
       );
       response.send(html);
-    });
+    }
+
+    
   });
 })
 
 // create를 get 방식으로 전송할 때 라우터
-app.get('/create', (request, response) =>{
-  fs.readdir('./data', function(error, filelist){
-    var title = 'WEB - create';
-    var list = template.list(filelist);
-    var html = template.HTML(title, list, `
-      <form action="/create" method="post">
-        <p><input type="text" name="title" placeholder="title"></p>
-        <p>
-          <textarea name="description" placeholder="description"></textarea>
-        </p>
-        <p>
-          <input type="submit">
-        </p>
-      </form>
-    `, '');
-    response.send(html);
-  });
+app.get('/create', (request, response) => {
+  var title = 'WEB - create';
+  var list = template.list(request.list);
+  var html = template.HTML(title, list, `
+    <form action="/create" method="post">
+      <p><input type="text" name="title" placeholder="title"></p>
+      <p>
+        <textarea name="description" placeholder="description"></textarea>
+      </p>
+      <p>
+        <input type="submit">
+      </p>
+    </form>
+  `, '');
+  response.send(html);
 });
 
 // post 방식으로 전송할 때 라우터 (create_process)
@@ -97,11 +112,10 @@ app.post('/create', (request, response)=> {
 });
 
 app.get('/update/:pageId', (request, response)=>{
-  fs.readdir('./data', function(error, filelist){
     var filteredId = path.parse(request.params.pageId).base;
     fs.readFile(`data/${filteredId}`, 'utf8', function(err, description){
       var title = request.params.pageId;
-      var list = template.list(filelist);
+      var list = template.list(request.list);
       var html = template.HTML(title, list,
         `
         <form action="/update/:pageId" method="post">
@@ -119,7 +133,6 @@ app.get('/update/:pageId', (request, response)=>{
       );
       response.send(html);
     });
-  });
 });
 
 app.post('/update/:pageId', (request, response)=>{
@@ -141,6 +154,17 @@ app.post('/delete', (request, response)=>{
   fs.unlink(`data/${filteredId}`, function(error){
     response.redirect('/');
   })
+})
+
+// 404 error
+app.use(function (req, res, next) {
+  res.status(404).send("Sorry can't find that!")
+})
+
+// error handler
+app.use(function (err, req, res, next) {
+  console.error(err.stack)
+  res.status(500).send('Something broke!')
 })
 
 app.listen(port, () => {
