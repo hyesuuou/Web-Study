fda2a8d9bcde3302e9124288ba1edff1cb4b5571
hyesuuou <68391767+hyesuuou@users.noreply.github.com>
2021-03-04 00:36:27 +0900

    egoing : WEB3-Cookie&Auth 7-8 practice

--- Code/egoing-web-nodejs-cookie/nodejs/cookie.js
+++ Code/egoing-web-nodejs-cookie/nodejs/cookie.js
@@ -10,8 +10,12 @@ http.createServer((req, res)=>{
     res.writeHead(200, {
         'Set-Cookie':[
             'yummy_cookie=choco', 
-            'tasty_cookie=strawberry'
+            'tasty_cookie=strawberry',
             `Permanent=cookies; Max-age=${60*60*24*30}`,
+            'Secure=Secure; Secure',
+            'HttpOnly=HttpOnly; HttpOnly',
+            'Path=Path; Path=/cookie',
+            'Domain=Domain; Domain=o2.org',
         ]
     });
     res.end('cookie!!');
